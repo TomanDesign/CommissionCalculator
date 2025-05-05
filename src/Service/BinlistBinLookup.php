e95ea545878cf5d06bfffedd216dc59fc79e7157
Tomasz Stolarczyk <tomasz@indigo-nails.com>
2025-05-05 22:56:35 +0200

    handle errors of too many requests

--- src/Service/BinlistBinLookup.php
+++ src/Service/BinlistBinLookup.php
@@ -6,8 +6,16 @@ use RuntimeException;
 
 class BinlistBinLookup implements BinLookupInterface
 {
+    private array $binCache = [];
+
     public function getCountryCode(string $bin): ?string
     {
+        if (isset($this->binCache[$bin])) {
+            return $this->binCache[$bin];
+        }
+
+        usleep(5000000);
+
         $url = 'https://lookup.binlist.net/' . urlencode($bin);
 
         $ch = curl_init($url);
@@ -30,11 +38,19 @@ class BinlistBinLookup implements BinLookupInterface
         $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
 
+        if ($httpCode === 429) {
+            throw new RuntimeException("Rate limit exceeded (HTTP 429) while querying BIN $bin.
+                                        Please slow down requests or implement persistent caching.");
+        }
+
         if ($httpCode !== 200 || !$response) {
             throw new RuntimeException("Binlist lookup failed for BIN $bin (HTTP $httpCode)");
         }
 
         $data = json_decode($response, true);
-        return $data['country']['alpha2'] ?? null;
+        $countryCode = $data['country']['alpha2'] ?? null;
+
+        $this->binCache[$bin] = $countryCode;
+        return $countryCode;
     }
 }
